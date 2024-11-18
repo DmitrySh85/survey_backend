0846e821be803180df3c90f93f2430f5ae2cffef
DSHakirov <shakirovdominatus@gmail.com>
2024-11-18 19:26:17 +0300

    fix script

--- survey_backend/add_data_to_db.py
+++ survey_backend/add_data_to_db.py
@@ -33,10 +33,10 @@ def insert_df_to_db(df: pd.DataFrame) -> dict:
     for _, row in df.iterrows():
         row_values = [None if pd.isna(value) else value for value in row.values.tolist()]
         text = row_values[0]
-        first_answer = str(row_values[2]) if row_values[1] else ""
-        second_answer = str(row_values[3]) if row_values[2] else ""
-        third_answer = str(row_values[4]) if row_values[3] else ""
-        fourth_answer = str(row_values[5]) if row_values[4] else ""
+        first_answer = str(row_values[1]) if row_values[1] else ""
+        second_answer = str(row_values[2]) if row_values[2] else ""
+        third_answer = str(row_values[3]) if row_values[3] else ""
+        fourth_answer = str(row_values[4]) if row_values[4] else ""
         valid_answer_number = int(row_values[5])
         description = row_values[6]
 
@@ -55,4 +55,4 @@ def insert_df_to_db(df: pd.DataFrame) -> dict:
 
 if __name__ == "__main__":
     insert_questions_to_db()
-    insert_users_to_db()
+    #insert_users_to_db()
