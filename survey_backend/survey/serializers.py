258d1833108a90de6f885f5cf792464f99ec2982
DSHakirov <shakirovdominatus@gmail.com>
2024-11-26 14:52:37 +0300

    CRM-734

--- survey_backend/survey/serializers.py
+++ survey_backend/survey/serializers.py
@@ -1,8 +1,19 @@
-from rest_framework.serializers import ModelSerializer
-from .models import Question
+from rest_framework.serializers import ModelSerializer, Serializer, UUIDField
+from .models import Question, DailySurveyCounter
 
 
 class QuestionSerializer(ModelSerializer):
     class Meta:
         model = Question
         fields = "__all__"
+
+
+class DailySurveyCounterSerializer(ModelSerializer):
+    class Meta:
+        model = DailySurveyCounter
+        fields = "__all__"
+
+    
+class IncreaseDailySurveyCounterSerializer(Serializer):
+
+    employee = UUIDField()
--- survey_backend/survey/urls.py
+++ survey_backend/survey/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import QuestionListAPIView
+from .views import (
+    QuestionListAPIView,
+    CreateDailySurveyCounter,
+    IncreaseDayliSurveyCounter
+)
 
 
 urlpatterns = [
     path("questions/", QuestionListAPIView.as_view(), name="questions"),
+    path("attempts/", CreateDailySurveyCounter.as_view(), name="create_counter"),
+    path("attempts/increase/", IncreaseDayliSurveyCounter.as_view(), name="increase_counter"),
 ]
